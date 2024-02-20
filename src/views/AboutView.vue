ced2bf05699d741f9d2a5a73c55f1836ed0fe2c1
IvanSeguraRomero <a27312@svalero.com>
2024-02-20 20:59:13 +0100

    CardView en progreso

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+    import { onMounted } from 'vue';
+    onMounted(() =>{
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    })
+</script>
 <template>
     
    <main>
