179f6ade37cd6e36af541efce04043fb57f0e330
GGSZGZ <guillermo.garcia@fundacionmontessori.com>
2024-03-10 17:35:34 +0100

    Dockerizado con exito

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 import AboutView from "../views/AboutView.vue"
-import ContactView from "@/views/ContactView.vue";
-import CardView from "@/views/CardView.vue";
-import TicketsView from "@/views/TicketView.vue";
-import SuccesView from "@/views/SuccesView.vue";
-import AdminView from "@/views/AdminView.vue";
-import RegisterView from "@/views/RegisterView.vue";
+import ContactView from "../views/ContactView.vue";
+import CardView from "../views/CardView.vue";
+import TicketsView from "../views/TicketView.vue";
+import SuccesView from "../views/SuccesView.vue";
+import AdminView from "../views/AdminView.vue";
+import RegisterView from "../views/RegisterView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
